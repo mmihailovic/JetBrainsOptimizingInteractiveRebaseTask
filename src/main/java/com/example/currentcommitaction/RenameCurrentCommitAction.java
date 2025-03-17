ddbc13e86198f52a6cfb856ae1da92e03b146768
Marko Mihailovic <marko.mihailovic67@yahoo.com>
2025-03-17 16:39:02 +0100

    implement rename commit button

---
+++ src/main/java/com/example/currentcommitaction/RenameCurrentCommitAction.java
@@ -0,0 +1,12 @@
+package com.example.currentcommitaction;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class RenameCurrentCommitAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
